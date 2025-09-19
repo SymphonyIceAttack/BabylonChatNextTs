63e94ce4f26415dc131af2355e5fd44a5792adf5
SymphonyIceAttack <1994370030@qq.com>
2025-09-19 17:53:52 +0800

    use biome lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,47 @@
 "use client";
-export default function Home() {
-	return <div>nothing</div>;
-}
+
+import { MessageContent, Message } from "@/components/ai-elements/message";
+import {
+	Conversation,
+	ConversationScrollButton,
+	ConversationContent,
+} from "@/components/ai-elements/conversation";
+import { Response } from "@/components/ai-elements/response";
+
+import { useChat } from "@ai-sdk/react";
+
+const ResponseDemo = () => {
+	const { messages } = useChat();
+
+	return (
+		<div className="max-w-4xl mx-auto p-6 relative size-full rounded-lg border h-[600px]">
+			<div className="flex flex-col h-full">
+				<Conversation>
+					<ConversationContent>
+						{messages.map((message) => (
+							<Message from={message.role} key={message.id}>
+								<MessageContent>
+									{message.parts.map((part, i) => {
+										switch (part.type) {
+											case "text": // we don't use any reasoning or tool calls in this example
+												return (
+													<Response key={`${message.id}-${i}`}>
+														{part.text}
+													</Response>
+												);
+											default:
+												return null;
+										}
+									})}
+								</MessageContent>
+							</Message>
+						))}
+					</ConversationContent>
+					<ConversationScrollButton />
+				</Conversation>
+			</div>
+		</div>
+	);
+};
+
+export default ResponseDemo;
